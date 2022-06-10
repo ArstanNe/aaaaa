522f5fd5eefb76f2e1b6619f95427dfd8ad98881
ArstanNe <107036975+ArstanNe@users.noreply.github.com>
2022-06-10 10:43:22 +0600

    asd

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Батыржанов Арыстан и Калей Аружан 
+                </div>
+
+            </div>
+        </div>
+    </section>
